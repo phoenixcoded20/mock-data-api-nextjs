5c6ac98e49be908504cb9dbb87f9d611f0fac28c
CodedThemes <77041536+phoenixcoded20@users.noreply.github.com>
2023-04-06 12:04:37 +0530

    Update cors.js

--- src/utils/cors.js
+++ src/utils/cors.js
@@ -5,8 +5,8 @@ import initMiddleware from './init-middleware';
 const cors = initMiddleware(
   // You can read more about the available options here: https://github.com/expressjs/cors#configuration-options
   Cors({
-    // Only allow requests with GET, POST and OPTIONS
-    methods: ['GET', 'POST', 'OPTIONS']
+    methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
+    origin: '*',
   })
 );
 
