38ee4acdce363bbcfdd31af893a1b2ac0ac8c31b
Tushar <tushar.savsaviya@phoenixcoded.co>
2025-10-09 16:24:50 +0530

    changelog api changes

--- src/pages/api/change-log/list.js
+++ src/pages/api/change-log/list.js
@@ -3,5 +3,8 @@ import changeLog from 'data/change-log.json';
 
 export default async function handler(req, res) {
   await cors(req, res);
-  res.status(200).json({ changeLog });
+
+  const reversedChangeLog = [...changeLog].reverse();
+
+  return res.status(200).json({ changeLog: reversedChangeLog });
 }
